ad6c4e4d0be8778c307367abab64582b394e2af0
tide_inokuchi <inokuchi@tidedev.jp>
2017-10-07 08:11:46 +0900

    fix right prompt

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -2,13 +2,10 @@ function __trident_custom_timestamp -S -d 'Show the current timestamp'
   [ "$theme_display_date" = "no" ]; and return
   set -q theme_date_format
     or set -l theme_date_format "+%H:%M:%S - %m/%d"
-  echo -n '  '
-  date $theme_date_format
+  return $trident_color_green( date $theme_date_format )
 end
 
-function fish_right_prompt -d 'bobthefish is all about the right prompt'
-  set_color $fish_color_cwd
+function fish_right_prompt -d 'simple timestamp'
   __trident_custom_timestamp
-  set_color normal
 end
 
