9ee12a504cfc22c815b4e25a123a597024919a4d
tide_inokuchi <inokuchi@tidedev.jp>
2017-10-07 08:14:36 +0900

    fix

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -2,7 +2,7 @@ function __trident_custom_timestamp -S -d 'Show the current timestamp'
   [ "$theme_display_date" = "no" ]; and return
   set -q theme_date_format
     or set -l theme_date_format "+%H:%M:%S - %m/%d"
-  return $trident_color_green( date $theme_date_format )
+  echo $trident_color_green( date $theme_date_format )
 end
 
 function fish_right_prompt -d 'simple timestamp'
